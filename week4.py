e7fcd8a9241ebf18ea472ef496faf00762c85700
clewman <cheryllewman@gmail.com>
2022-02-11 08:33:26 -0800

    part 2

--- week4.py
+++ week4.py
@@ -11,5 +11,20 @@ else:
     newtotal = ottotal * r * 1.5
     total = 40 * r + newtotal
 
-print("What's your name")
 print(total);
+
+#part 2
+score = input("Enter Score: ")
+sc = float(score)
+if sc > 1:
+    print("Out of range.");
+elif sc >= 0.9:
+    print("A")
+elif sc >= 0.8:
+    print("B")
+elif sc >= 0.7:
+    print("C")
+elif sc >= 0.6:
+    print("D")
+elif sc < 0.6:
+    print("F")
